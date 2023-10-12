ed38692f9b46d4a2ea7f90abbc4c0826fd557976
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-10-12 09:22:07 +0800

    added distributor

--- client/src/components/Add-new-distributor-form.jsx
+++ client/src/components/Add-new-distributor-form.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import barcodeImg from "../assets/barcode.svg";
+import axios from "axios";
 
 const AddNewDistributorContainer = ({ setIsShowForm }) => {
+  const [distributorName, setDistributorName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactPerson, setContactPerson] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+
+  const handleAddDistributor = async (event) => {
+    event.preventDefault();
+    try {
+      const url = "http://localhost:3000/distributor/add-distributor";
+      const { data } = await axios.post(url, {
+        distributorName,
+        address,
+        email,
+        contactPerson,
+        contactNumber,
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full h-full absolute inset-0 flex justify-center items-center bg-black bg-opacity-40 backdrop-blur-[2px]">
       <div className="flex flex-col bg-white p-10 rounded-lg w-[50rem] gap-14 shadow-components">
@@ -9,16 +34,18 @@ const AddNewDistributorContainer = ({ setIsShowForm }) => {
           <h1 className="font-black text-xl">BARCODE SCANNER</h1>
         </div>
         <div>
-          <form className="flex flex-col gap-10">
+          <form
+            className="flex flex-col gap-10"
+            onSubmit={handleAddDistributor}
+          >
             <div className="flex flex-col gap-5">
               <div className="flex gap-3 justify-between items-center">
-                <label
-                  htmlFor="distributor-name"
-                  className="font-bold text-lg"
-                >
+                <label htmlFor="distributor-name" className="font-bold text-lg">
                   DISTRIBUTOR NAME:
                 </label>
                 <input
+                  onChange={(e) => setDistributorName(e.target.value)}
+                  value={distributorName}
                   type="text"
                   className="w-[70%] outline-1 outline outline-green-400 rounded-md p-2"
                 />
@@ -28,6 +55,8 @@ const AddNewDistributorContainer = ({ setIsShowForm }) => {
                   ADDRESS:
                 </label>
                 <textarea
+                  onChange={(e) => setAddress(e.target.value)}
+                  value={address}
                   name="address"
                   className="w-[70%] outline-1 outline outline-green-400 rounded-md p-2"
                 ></textarea>
@@ -37,6 +66,8 @@ const AddNewDistributorContainer = ({ setIsShowForm }) => {
                   EMAIL:
                 </label>
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                   type="text"
                   className="w-[70%] outline-1 outline outline-green-400 rounded-md p-2"
                 />
@@ -44,13 +75,12 @@ const AddNewDistributorContainer = ({ setIsShowForm }) => {
             </div>
             <div className="flex flex-col gap-5">
               <div className="flex gap-3 justify-between items-center">
-                <label
-                  htmlFor="contact-person"
-                  className="font-bold text-lg"
-                >
+                <label htmlFor="contact-person" className="font-bold text-lg">
                   CONTACT PERSON:
                 </label>
                 <input
+                  onChange={(e) => setContactPerson(e.target.value)}
+                  value={contactPerson}
                   type="text"
                   className="w-[70%] outline-1 outline outline-green-400 rounded-md p-2"
                 />
@@ -60,6 +90,8 @@ const AddNewDistributorContainer = ({ setIsShowForm }) => {
                   CONTACT NUMBER:
                 </label>
                 <input
+                  onChange={(e) => setContactNumber(e.target.value)}
+                  value={contactNumber}
                   type="text"
                   className="w-[70%] outline-1 outline outline-green-400 rounded-md p-2"
                 />
